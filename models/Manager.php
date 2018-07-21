cf2b4a194ea274069dfb13a89b71acab93abcc44
yohannzaoui <yohannzaoui@gmail.com>
2018-07-21 10:33:48 +0200

    create branch test

--- models/Manager.php
+++ models/Manager.php
@@ -2,9 +2,9 @@
 
 class Manager
 {
-  
+  protected $db;
 
-  public function dbConnect()
+  protected function dbConnect()
   {
     $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','root','');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
