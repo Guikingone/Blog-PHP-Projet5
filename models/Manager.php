6b859b08e57f4aeadff15a4282a5aeed3f00646a
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2018-08-16 14:29:31 +0200

    refactor(PDO): PDO connection refactoring

--- models/Manager.php
+++ models/Manager.php
@@ -1,15 +1,26 @@
 <?php
 
-abstract class Manager {
+namespace Core;
 
+final class Manager 
+{
+    /**
+     * @var \PDO|null
+     */
+    private $pdo = null;
+
+    // This method should be public has is gonna been used by the repository.
+    public function getDb() 
+    {    
+        $config = $this->loadConfiguration('database.php');
+        
+        return $this->pdo = new \PDO($config['driver'].':host='.$config['host'].';dbname='.$config['dbname'].';charset=utf8', $config['username'], $config['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    }
     
-    private $_db;
-
-    protected function getDb() {
-        if ($this->_db == null) {
-            $this->_db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','root', '',
-                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }
-            return $this->_db;
+    private function loadConfiguration(filename)
+    {
+        // Maybe we can load the database credentials from a file ? 
+        // This class should be placed under the etc folder as it a "core" class.
+        return require_once __DIR__.'/../config/'.$filename;
     }
 }
